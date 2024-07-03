1b9572362ad3ba4fe097bd489fc9605f46c10c7d
Ahmed Sayed <ahmed.mohameeed733@gmail.com>
2024-07-03 16:46:47 +0300

    signup page first commit

---
+++ src/components/Signup/Signup.jsx
@@ -0,0 +1,46 @@
+import "./Signup.css";
+import React from "react";
+import AppleSVG from "../SVG/AppleSVG";
+import AutumnSVG from "../SVG/AutumnSVG";
+import GoogleSVG from "../SVG/GoogleSVG";
+import FacebookSVG from "../SVG/FacebookSVG";
+import SignupForm from "../SignupForm/SignupForm";
+import ErpSvg from "../SVG/ErpSVG"
+
+const Signup = () => {
+  return (
+    <div className="signup-from">
+      <div className="signup-froml">
+          <AutumnSVG />
+          </div>
+      <div className="signup-fromr">
+        <div className="form-header">
+          <ErpSvg />
+          <p>Create Account</p>
+        </div>
+        <div className="form">
+          <SignupForm />
+        </div>
+        <div className="form-footer">
+          <p className="text-legend">- OR -</p>
+          <div className="social-icons">
+            <span className="social-icon">
+              <GoogleSVG />
+            </span>
+            <span className="social-icon">
+              <FacebookSVG />
+            </span>
+            <span className="social-icon">
+              <AppleSVG />
+            </span>
+          </div>
+          <p className="text-footer">
+            Already have an account? <span className="text-olive">Login</span>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Signup;
